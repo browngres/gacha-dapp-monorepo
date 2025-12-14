c6e7c53a400555b541a384f6ada539035bfd7913
Rainy <yusi_rainy@outlook.com>
2025-12-14 20:31:13 +0800

    test(contract): 测试稀有度的计算

--- packages/contract/test/GachaPool-rarity.ts
+++ packages/contract/test/GachaPool-rarity.ts
@@ -36,32 +36,108 @@ describe("GachaPool Rarity Unit Tests", function () {
     async function () {
       const reqId = await gachaOneTime()
       // 给出指定的随机数
-      await expect(vrf.fulfillRandomWordsWithOverride(reqId,gachaPool.target,randomWords)).to.emit(gachaPool,"RandomFulfilled")
+      await expect(vrf.fulfillRandomWordsWithOverride(reqId, gachaPool.target, randomWords)).to.emit(
+        gachaPool,
+        "RandomFulfilled",
+      )
       // 检查结果
       const result = await gachaPool.getResult(reqId)
-      console.log("result of reqId:", reqId, result)
+      // console.log("result of reqId:", reqId, result)
       expect(result[0]).equal(1) // numWords
-      expect(result[1]).to.have.members(randomWords); // words
+      expect(result[1]).to.have.members(randomWords) // words
       expect(result[2]).to.have.members(expected) // rarity
-    };
+    }
 
   describe("Default percentages", function () {
     before("Pause and set percentages [2, 8, 10, 20, 60]", async function () {
       await gachaPool.pause()
-      console.log("Paused")
+      // console.log("Paused")
       await gachaPool.setPercentage([2, 8, 10, 20, 60])
       await gachaPool.unpause()
-      console.log("Unpaused")
+      // console.log("Unpaused")
     })
 
-    it("correctly give rarity N", testRarity([999027n], [4n]))
-    // it("correctly give rarity UR", testRarity([99999], [0]))
+    it("correctly gives rarity N", testRarity([999000n], [4n])) // [0,60)
+    it("correctly gives rarity R", testRarity([999060n], [3n])) // [60,80)
+    it("correctly gives rarity R", testRarity([999079n], [3n]))
+    it("correctly gives rarity SR", testRarity([999080n], [2n])) // [80-90)
+    it("correctly gives rarity SR", testRarity([999089n], [2n])) //
+    it("correctly gives rarity SSR", testRarity([999090n], [1n])) // [90,98)
+    it("correctly gives rarity SSR", testRarity([999097n], [1n])) //
+    it("correctly gives rarity UR", testRarity([999098n], [0n])) // [98,100)
+    it("correctly gives rarity UR", testRarity([999099n], [0n]))
+  })
 
+  describe("Percentages whit zero in the 1st position", function () {
+    before("Pause and set percentages [0, 10, 10, 20, 60]", async function () {
+      await gachaPool.pause()
+      await gachaPool.setPercentage([0, 10, 10, 20, 60])
+      await gachaPool.unpause()
+    })
+
+    it("correctly gives rarity N", testRarity([999000n], [4n])) // [0,60)
+    it("correctly gives rarity N", testRarity([999059n], [4n]))
+    it("correctly gives rarity R", testRarity([999060n], [3n])) // [60,80)
+    it("correctly gives rarity R", testRarity([999079n], [3n]))
+    it("correctly gives rarity SR", testRarity([999080n], [2n])) // [80-90)
+    it("correctly gives rarity SR", testRarity([999089n], [2n]))
+    it("correctly gives rarity SSR", testRarity([999090n], [1n])) // [90,100)
+    it("correctly gives rarity SSR", testRarity([999099n], [1n]))
+  })
+
+  describe("Percentages whit zero in the middle", function () {
+    before("Pause and set percentages [2, 8, 0, 30, 60]", async function () {
+      await gachaPool.pause()
+      await gachaPool.setPercentage([2, 8, 0, 30, 60])
+      await gachaPool.unpause()
+    })
+
+    it("correctly gives rarity N", testRarity([999000n], [4n])) // [0,60)
+    it("correctly gives rarity N", testRarity([999059n], [4n]))
+    it("correctly gives rarity R", testRarity([999060n], [3n])) // [60,90)
+    it("correctly gives rarity R", testRarity([999089n], [3n]))
+    it("correctly gives rarity SSR", testRarity([999090n], [1n])) // [90,98)
+    it("correctly gives rarity SSR", testRarity([999097n], [1n]))
+    it("correctly gives rarity UR", testRarity([999098n], [0n])) // [98,100)
+    it("correctly gives rarity UR", testRarity([999099n], [0n]))
+  })
+
+  describe("Percentages whit zero at the end", function () {
+    before("Pause and set percentages [2, 8, 0, 90, 0]", async function () {
+      await gachaPool.pause()
+      await gachaPool.setPercentage([2, 8, 0, 90, 0])
+      await gachaPool.unpause()
+    })
+
+    it("correctly gives rarity R", testRarity([999000n], [3n])) // [0,90)
+    it("correctly gives rarity R", testRarity([999089n], [3n]))
+    it("correctly gives rarity SSR", testRarity([999090n], [1n])) // [90,98)
+    it("correctly gives rarity SSR", testRarity([999097n], [1n]))
+    it("correctly gives rarity UR", testRarity([999098n], [0n])) // [98,100)
+    it("correctly gives rarity UR", testRarity([999099n], [0n]))
+  })
+
+  describe("Percentages whit only 100% UR", function () {
+    before("Pause and set percentages [100, 0, 0, 0, 0]", async function () {
+      await gachaPool.pause()
+      await gachaPool.setPercentage([100, 0, 0, 0, 0])
+      await gachaPool.unpause()
+    })
+
+    it("correctly gives rarity UR", testRarity([999000n], [0n])) // [0,100)
+    it("correctly gives rarity UR", testRarity([999050n], [0n]))
+    it("correctly gives rarity UR", testRarity([999099n], [0n]))
+  })
+
+  describe("Percentages whit only 100% UR", function () {
+    before("Pause and set percentages [0, 0, 0, 0, 100]", async function () {
+      await gachaPool.pause()
+      await gachaPool.setPercentage([0, 0, 0, 0, 100])
+      await gachaPool.unpause()
+    })
+
+    it("correctly gives rarity N", testRarity([999000n], [4n])) // [0,100)
+    it("correctly gives rarity N", testRarity([999050n], [4n]))
+    it("correctly gives rarity N", testRarity([999099n], [4n]))
   })
 })
-
-// 测试 rarity
-// 1. 5种都有  [2, 8, 10, 20, 60]
-// 2. 第一个为0 [0, 10, 10, 20, 60]
-// 3. 中间为0  [2, 8, 0, 30, 60]
-// 4. 最后有0  [2, 8, 0, 90, 0]
