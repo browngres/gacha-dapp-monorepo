b82df688a9a02c3b3b5b45a2d6fb08342b383382
Rainy <yusi_rainy@outlook.com>
2025-12-21 16:13:47 +0800

    feat(app): 单抽十连的组件状态切换

--- packages/app/src/components/gachaTab.tsx
+++ packages/app/src/components/gachaTab.tsx
@@ -2,8 +2,9 @@ import { type BaseError } from "wagmi";
 
 import { formatUnits } from "viem";
 import { getPoolInfo } from "./read-gacha";
+import { useEffect, useState } from "react";
 
-export function PoolInfoCard() {
+export function PoolInfoCard({ setIsBlurred, setIsTen }) {
   // 卡池展示
   // TODO 将来从 pool manager 读取 pool 信息
   const { data, error, isPending } = getPoolInfo();
@@ -74,12 +75,39 @@ export function PoolInfoCard() {
         <progress className="progress w-56 mx-auto" value={0} max="100"></progress>
         {/* 单抽、十连按钮 */}
         <div className="card-actions flex justify-between my-2 mx-6">
-          <button className="btn btn-primary">GachaOne</button>
+          <button
+            className="btn"
+            onClick={() => {
+              document!.getElementById("gacha_hint_modal")!.showModal();
+              setIsTen(false);
+            }}
+          >
+            GachaOne
+          </button>
+          {/* 消息提示框 */}
+          <dialog id="gacha_hint_modal" className="modal">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">请在按照右侧继续操作</p>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+              <button onClick={() => setIsBlurred(false)}>close</button>
+            </form>
+          </dialog>
+
           <div className="indicator">
             <span className="indicator-item badge badge-soft badge-success italic">
               {100 - Number(discountGachaTen)}% off!
             </span>
-            <button className="btn btn-accent">GachaTen</button>
+            <button
+              className="btn btn-accent"
+              onClick={() => {
+                document!.getElementById("gacha_hint_modal")!.showModal();
+                setIsTen(true);
+              }}
+            >
+              GachaTen
+            </button>
           </div>
         </div>
       </div>
@@ -87,29 +115,42 @@ export function PoolInfoCard() {
   );
 }
 
-function GachaWorkflow() {
+function GachaWorkflow({ isBlurred, isTen }) {
   // 抽卡流程
+  const [currStep, setCurrStep] = useState(0);
+
   return (
-    <div className="card grow bg-base-100 shadow-sm blur-sm">
+    <div className={`card grow bg-base-100 shadow-sm ${isBlurred && "blur-sm pointer-events-none select-none"}`}>
       <div className="card-body flex-row">
         {/* 进度条 */}
         <ul className="steps steps-vertical h-full w-1/3 font-bold">
-          <li className="step">
+          <li className={`step ${currStep > 0 && "step-primary"}`}>
             <span>Gacha</span>
           </li>
-          <li className="step">
+          <li className={`step ${currStep > 1 && "step-primary"}`}>
             <span>Signature</span>
           </li>
-          <li className="step">
+          <li className={`step ${currStep > 2 && "step-primary"}`}>
             <span>Fulfill VRF</span>
           </li>
-          <li className="step">
+          <li className={`step ${currStep > 3 && "step-primary"}`}>
             <span>Get Result</span>
           </li>
         </ul>
         {/* 右侧内容 */}
         <ul className="grid grid-rows-4 place-items-center">
-          <li>等待抽卡结果（读取 event gachaOne）</li>
+          <li>
+            {isTen ? (
+              <button className="btn btn-soft btn-warning" onClick={() => {}}>
+                十连
+              </button>
+            ) : (
+              <button className="btn btn-soft btn-warning" onClick={() => {}}>
+                单抽
+              </button>
+            )}
+            抽卡+等待交易结果（读取 event gachaOne）
+          </li>
           <li>等待后端返回签名</li>
           <li>等待随机数 fulfill（读取 event RandomFulfilled）</li>
           <li>显示抽卡结果</li>
@@ -120,12 +161,15 @@ function GachaWorkflow() {
 }
 
 export function GachaTab() {
+  const [isBlurred, setIsBlurred] = useState<boolean>(true);
+  const [isTen, setIsTen] = useState<boolean>(false);
+
   return (
     <div className="container flex">
       {/* 卡池展示 */}
-      <PoolInfoCard />
+      <PoolInfoCard setIsBlurred={setIsBlurred} setIsTen={setIsTen} />
       {/* 抽卡流程 */}
-      <GachaWorkflow />
+      <GachaWorkflow isBlurred={isBlurred} isTen={isTen} key={Number(isTen)} />
     </div>
   );
 }
