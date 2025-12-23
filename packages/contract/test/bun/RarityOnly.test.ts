3f9a92d888c1a4785d792055c3f611c5cea3b631
Rainy <yusi_rainy@outlook.com>
2025-12-23 19:29:03 +0800

    test(contract): 添加使用 bun  的测试 RarityOnly.test.ts。 fix RarityOnly.ts

--- packages/contract/test/bun/RarityOnly.test.ts
+++ packages/contract/test/bun/RarityOnly.test.ts
@@ -1,15 +1,16 @@
-import { expect } from "chai"
+// import { expect } from "chai"
+import { describe, expect, test } from "bun:test"
 import { network } from "hardhat"
 
 describe("RarityOnly Unit Tests", function () {
-  it("Should successfully", async function () {
+  test("Should successfully", async function () {
     const { ethers } = await network.connect()
     const rarity = await ethers.deployContract("RarityOnly")
     const words = [999088n]
     await rarity.fulfillRandomWords(123, words)
     const result = await rarity.getResult(123)
-    expect(result[0]).equal(1) // numWords
-    expect(result[1]).to.have.members(words) // words
-    expect(result[2]).to.have.members([2n]) // rarity
+    expect(result[0]).toEqual(1n) // numWords
+    expect(result[1]).toEqual(words) // words
+    expect(result[2]).toEqual([2n]) // rarity
   })
 })
